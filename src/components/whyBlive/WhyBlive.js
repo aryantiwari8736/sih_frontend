a5d98b5a696e17d483320b86ef6b512ec9ae7747
prabhat-021 <prabhatsahrawat010203@gmail.com>
2023-09-19 12:40:41 +0530

    Image Updated

--- src/components/whyBlive/WhyBlive.js
+++ src/components/whyBlive/WhyBlive.js
@@ -1,24 +1,27 @@
 import Card from "./Card";
-import img1 from "../../assets/luggage.png";
+import img1 from "../../assets/realTime.png";
+import img2 from "../../assets/emmision.png";
+import img3 from "../../assets/Notific.png";
+import img4 from "../../assets/Seat.png";
 import { ImLocation2 } from "react-icons/im";
 import styles from "./WhyBlive.module.css";
 
 const data = [
     {
         img: img1,
-        title: "Safety",
+        title: "Real Time Bus Track",
         text: "With our stringent safety checks, we ensure you travel completely safe"
     }, {
-        img: img1,
-        title: "Safety",
+        img: img2,
+        title: "Emission",
         text: "With our stringent safety checks, we ensure you travel completely safe"
     }, {
-        img: img1,
-        title: "Safety",
+        img: img3,
+        title: "Notification",
         text: "With our stringent safety checks, we ensure you travel completely safe"
     }, {
-        img: img1,
-        title: "Safety",
+        img: img4,
+        title: "Seat Availability",
         text: "With our stringent safety checks, we ensure you travel completely safe"
     }
 ]
@@ -32,7 +35,7 @@ export default function WhyBlive() {
             <div className='flex flex-wrap space-x-2 justify-evenly'>
                 {data.map((e) => <Card img={e.img} title={e.title} text={e.text} />)}
             </div>
-     
+
         </section>
     );
 }
